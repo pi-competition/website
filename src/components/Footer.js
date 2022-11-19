1afd4f3756f32e01c41b392238138217353e266e
Ogwen123 <owen.d.jones@btinternet.com>
2022-11-19 21:47:31 +0000

    fixed the stuff i messed up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 import Stack from '@mui/material/Stack'
 import React from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import GitHub from '@mui/icons-material/GitHub';
 
 const Footer = () => {
     return (
         <div className='footer flex justify-center align-bottom'>
             <Stack>
-                <h3 className='underline bold flex justify-center'>Links</h3>
                 <a
                     href="https://github.com/pi-competition"
                     target={"_blank"}
                     rel="noopener noreferrer"
-                >Github</a>
+                ><GitHubIcon /></a>
             </Stack>
         </div>
     )
