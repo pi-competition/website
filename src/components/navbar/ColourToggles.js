1f8c9be2defaca39dcae19781e9c7014d6ed004f
Ogwen123 <owen.d.jones@btinternet.com>
2022-11-19 19:52:49 +0000

    changed toggle colour icon

--- src/components/navbar/ColourToggles.js
+++ src/components/navbar/ColourToggles.js
@@ -1,52 +1,14 @@
-import React, { useState } from 'react'
-import ToggleButton from '@mui/material/ToggleButton'
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
-import { ThemeContext, themes } from '../../contexts/themeContext'
+import ToggleButton from "./ToggleButton"
+import React, { useState, useEffect } from 'react'
+import { ThemeContext } from '../../contexts/themeContext'
+
 
 const ColourToggles = () => {
-    const [aligment, setAlignment] = useState("dark");
-    const [darkMode, setDarkMode] = useState(true)
-
-    const handleAlignment = (event, newAlignment) => {
-        if (newAlignment !== null) {
-            setAlignment(newAlignment);
-        }
-    }
-
     return (
         <div className='colour-mode-buttons'>
             <ThemeContext.Consumer>
                 {({ changeTheme }) => (
-                    <ToggleButtonGroup
-                        color="primary"
-                        value={aligment}
-                        exclusive
-                        onChange={handleAlignment}
-                        aria-label="colour toggle"
-                    >
-                        <ToggleButton
-                            value="dark"
-                            aria-label='dark'
-                            id="colour-toggle-dark"
-                            onClick={(event) => {
-                                setDarkMode(true);
-                                changeTheme(themes.dark);
-                            }}
-                        >
-                            Dark
-                        </ToggleButton>
-                        <ToggleButton
-                            value="light"
-                            aria-label='light'
-                            id="colour-toggle-light"
-                            onClick={() => {
-                                setDarkMode(false);
-                                changeTheme(themes.light);
-                            }}
-                        >
-                            Light
-                        </ToggleButton>
-                    </ToggleButtonGroup>
+                    <ToggleButton changeTheme={changeTheme} />
                 )}
             </ThemeContext.Consumer>
         </div>
---
+++ src/components/navbar/ToggleButton.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import { themes } from "../../contexts/themeContext"
+import IconButton from '@mui/material/IconButton'
+import Brightness4Icon from "@mui/icons-material/Brightness4"
+import Brightness7Icon from "@mui/icons-material/Brightness7"
+
+const ToggleButton = ({ changeTheme }) => {
+    const [darkMode, setDarkMode] = useState(true)
+
+    useEffect(() => {
+        changeTheme(darkMode ? themes.dark : themes.light);
+    }, [darkMode])
+
+    return (
+        <div>
+            <IconButton
+                onClick={() => {
+                    setDarkMode(!darkMode);
+                }}
+                color="inherit"
+            >
+                {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+        </div>
+    )
+}
+
+export default ToggleButton
