3af3ee45ecca2b60d219564b2ec516fe1a9fc70f
Ogwen123 <owen.d.jones@btinternet.com>
2022-11-19 21:49:22 +0000

    mui error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import Stack from '@mui/material/Stack'
 import React from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
-import GitHub from '@mui/icons-material/GitHub';
 
 const Footer = () => {
     return (
