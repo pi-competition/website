12ec8dc78361526d159a59a265e48c813b46dd4e
Ogwen123 <owen.d.jones@btinternet.com>
2022-11-19 21:08:03 +0000

    centered buttons

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BsGithub } from "react-icons/bs"
 import { Link } from "react-router-dom"
 import "../index.css"
 
--- src/components/navbar/ColourToggles.js
+++ src/components/navbar/ColourToggles.js
@@ -1,5 +1,5 @@
 import ToggleButton from "./ToggleButton"
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { ThemeContext } from '../../contexts/themeContext'
 
 
