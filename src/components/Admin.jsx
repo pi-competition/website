a166c1ea0005076a8f44cff183559d15aa4f9f69
Ogwen123 <owen.d.jones@btinternet.com>
2023-03-02 17:36:54 +0000

    not broken anymore

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -5,10 +5,11 @@ import "../admin.css"
 import CarsStatus from './admin/CarsStatus';
 import config from "../config/config.json"
 import { Navigate, redirect } from 'react-router-dom';
+import { FOCUSABLE_SELECTOR } from '@testing-library/user-event/dist/utils';
 
 const Admin = () => {
     let baseURL;
-    const [token, setToken] = useState("");
+    const [token, setToken] = useState("temp");
     const [carData, setCarData] = useState();
     const [loading, setLoading] = useState(true);
     const [returnValue, setReturnValue] = useState();
@@ -23,54 +24,62 @@ const Admin = () => {
 
     //checking if they are logged in already
     useEffect(() => {
-        const savedToken = localStorage.getItem("admin-token")
-        let savedUnix = Math.floor(localStorage.getItem("admin-unix"))
-        console.log(savedUnix)
-        console.log(config.adminLoginPersistDuration)
-        if (savedToken !== null) {
-            setToken("temp")
-            let currentTime = Math.floor(new Date().getTime() / 1000)
-            console.log("diff" + (currentTime - savedUnix))
-            if (currentTime - savedUnix > config.adminLoginPersistDuration) {
-                localStorage.removeItem("admin-token")
-                localStorage.removeItem("admin-time")
-                return redirect("/admin/login")
-            }
-
-            const data = {
-                "password": savedToken
-            }
-            const url = baseURL + "verify"
-            const fetchOptions = {
-                method: "POST",
-                mode: "cors",
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            }
-            //post request
-            fetch(url, fetchOptions).then((result) => {
-                if (result.status === 204) {
-                    setToken(savedToken)
-                    setLoading(false)
-                    setReturnValue(true)
-                } else {
+        setTimeout(() => {
+            console.log("checking")
+            const savedToken = localStorage.getItem("admin-token")
+            let savedUnix = Math.floor(localStorage.getItem("admin-unix"))
+            if (savedToken !== null) {
+                console.log("checking2")
+                let currentTime = Math.floor(new Date().getTime() / 1000)
+                if (currentTime - savedUnix > config.adminLoginPersistDuration) {
                     localStorage.removeItem("admin-token")
                     localStorage.removeItem("admin-time")
+                    setReturnValue(false)
                 }
-            })
-                .catch(() => {
-                    console.log("Very bad error occured while verifying the saved password")
-                })
 
-        } else {
-            setReturnValue(false)
-        }
+                const data = {
+                    "password": savedToken
+                }
+                const url = baseURL + "verify"
+                const fetchOptions = {
+                    method: "POST",
+                    mode: "cors",
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data)
+                }
+                //post request
+                fetch(url, fetchOptions).then((result) => {
+                    if (result.status === 204) {
+                        setToken(savedToken)
+                        setLoading(false)
+                        setReturnValue(true)
+                    } else {
+                        localStorage.removeItem("admin-token")
+                        localStorage.removeItem("admin-time")
+                    }
+                })
+                    .catch(() => {
+                        console.log("Very bad error occured while verifying the saved password")
+                    })
+
+            } else {
+                console.log("checking3")
+                setReturnValue(false)
+                setToken("")
+            }
+        }, 1000)
     }, [])
 
+
+
     if (returnValue === false && !token) {
+        console.log(returnValue === false)
+        console.log("redirecting")
+        setTimeout(() => {
+        }, 300)
         return (<Navigate replace to="/admin/login" />)
     }
 
--- src/components/admin/Login.jsx
+++ src/components/admin/Login.jsx
@@ -34,6 +34,49 @@ const Login = () => {
         setCollapseOpen(true)
     }
 
+    useEffect(() => {
+        const savedToken = localStorage.getItem("admin-token")
+        let savedUnix = Math.floor(localStorage.getItem("admin-unix"))
+        if (savedToken !== null) {
+            let currentTime = Math.floor(new Date().getTime() / 1000)
+            if (currentTime - savedUnix > config.adminLoginPersistDuration) {
+                localStorage.removeItem("admin-token")
+                localStorage.removeItem("admin-time")
+                return
+            }
+
+            const data = {
+                "password": savedToken
+            }
+            const url = baseURL + "verify"
+            const fetchOptions = {
+                method: "POST",
+                mode: "cors",
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            }
+            //post request
+            fetch(url, fetchOptions).then((result) => {
+                if (result.status === 204) {
+                    setReturnValue(true)
+                } else {
+                    localStorage.removeItem("admin-token")
+                    localStorage.removeItem("admin-time")
+                }
+            })
+                .catch(() => {
+                    console.log("Very bad error occured while verifying the saved password")
+                })
+
+        } else {
+            console.log("checking3")
+            setReturnValue(false)
+        }
+    }, [])
+
     const login = async () => {
         setButtonState(true)
         const username = userField
@@ -54,15 +97,16 @@ const Login = () => {
             },
             body: JSON.stringify(data)
         }
-        let returnValue = false;
         //post request
         fetch(url, fetchOptions)
             .then((response) => {
                 if (response.status === 204) {
-                    console.log("ghdfg")
                     localStorage.setItem("admin-token", password)
                     localStorage.setItem("admin-unix", Math.floor(new Date().getTime() / 1000))
-                    returnValue = true
+                    setTimeout(() => {
+                        setReturnValue(true)
+                    }, 1000)
+
                 } else if (response.status === 401 || response.status === 403) {
                     response.json().then((response_json) => {
                         handleLoginError(response_json.message, response_json.error)
@@ -82,103 +126,95 @@ const Login = () => {
             if (e.keyCode === 13 && e.shiftKey === false) {
                 e.preventDefault();
                 const tempVal = login()
-                console.log("fgjkhdfjg" + tempVal)
                 setReturnValue(tempVal)
             }
         } else {
             e.preventDefault()
             const tempVal = login()
-            console.log("fgjkhdfjg" + tempVal)
             setReturnValue(tempVal)
         }
     }
 
     if (returnValue) {
-        console.log("success")
+        return (<Navigate replace to="/admin" />)
+    } else {
         return (
-            <Navigate replace to="/admin" />
+            <div className='login-container flex justify-center items-center'>
+                <div className='login-border'>
+                    <Stack
+                        component="form"
+                        noValidate
+                        autoComplete='off'
+                        className="admin-input-stack"
+                        spacing={2}
+                    >
+                        <h1 className='admin-login-label text-xl font-bold'>Admin Page Login</h1>
+                        <TextField
+                            id="username-input"
+                            label="Username"
+                            variant='filled'
+                            value={userField}
+                            onKeyUp={(e) => handleLoginAction(e, true)}
+                            onChange={(newValue) => {
+                                setUserField(newValue.target.value)
+                            }}
+                        />
+                        <TextField
+                            id="password-input"
+                            label="Password"
+                            type="password"
+                            variant='filled'
+                            value={passField}
+                            onKeyUp={(e) => handleLoginAction(e, true)}
+                            onChange={(newValue) => {
+                                setPassField(newValue.target.value)
+                            }}
+                        />
+                        <Button
+                            id="enter-login-button"
+                            variant="contained"
+                            onClick={(e) => handleLoginAction(e, false)}
+                            disabled={buttonState}
+                        >
+                            Enter
+                        </Button>
+                        <Collapse
+                            in={collapseOpen}
+                            addEndListener={() => {
+                                setTimeout(() => {
+                                    setCollapseOpen(false)
+                                }, ALERT_DURATION);
+                            }}
+                            className="login-collapse"
+                        >
+                            <Alert
+                                className='login-alert'
+                                variant="filled"
+                                severity={"error"}
+                                action={
+                                    <IconButton
+                                        aria-label="close"
+                                        color="inherit"
+                                        size="small"
+                                        onClick={() => {
+                                            setCollapseOpen(false);
+                                        }}
+                                    >
+                                        <CloseIcon fontSize="inherit" />
+                                    </IconButton>
+                                }
+                                sx={{ mb: 2 }}
+                            >
+                                <AlertTitle>{loginErrorTitle}</AlertTitle>
+                                {loginErrorMessage}
+                            </Alert>
+                        </Collapse>
+                    </Stack>
+                </div>
+            </div>
         )
     }
 
-
-
-
-
-    return (
-        <div className='login-container flex justify-center items-center'>
-            <div className='login-border'>
-                <Stack
-                    component="form"
-                    noValidate
-                    autoComplete='off'
-                    className="admin-input-stack"
-                    spacing={2}
-                >
-                    <h1 className='admin-login-label text-xl font-bold'>Admin Page Login</h1>
-                    <TextField
-                        id="username-input"
-                        label="Username"
-                        variant='filled'
-                        value={userField}
-                        onKeyUp={(e) => handleLoginAction(e, true)}
-                        onChange={(newValue) => {
-                            setUserField(newValue.target.value)
-                        }}
-                    />
-                    <TextField
-                        id="password-input"
-                        label="Password"
-                        type="password"
-                        variant='filled'
-                        value={passField}
-                        onKeyUp={(e) => handleLoginAction(e, true)}
-                        onChange={(newValue) => {
-                            setPassField(newValue.target.value)
-                        }}
-                    />
-                    <Button
-                        id="enter-login-button"
-                        variant="contained"
-                        onClick={(e) => handleLoginAction(e, false)}
-                        disabled={buttonState}
-                    >
-                        Enter
-                    </Button>
-                    <Collapse
-                        in={collapseOpen}
-                        addEndListener={() => {
-                            setTimeout(() => {
-                                setCollapseOpen(false)
-                            }, ALERT_DURATION);
-                        }}
-                        className="login-collapse"
-                    >
-                        <Alert
-                            className='login-alert'
-                            variant="filled"
-                            severity={"error"}
-                            action={
-                                <IconButton
-                                    aria-label="close"
-                                    color="inherit"
-                                    size="small"
-                                    onClick={() => {
-                                        setCollapseOpen(false);
-                                    }}
-                                >
-                                    <CloseIcon fontSize="inherit" />
-                                </IconButton>
-                            }
-                            sx={{ mb: 2 }}
-                        >
-                            <AlertTitle>{loginErrorTitle}</AlertTitle>
-                            {loginErrorMessage}
-                        </Alert>
-                    </Collapse>
-                </Stack>
-            </div>
-        </div>
-    )
 }
 
 export default Login
